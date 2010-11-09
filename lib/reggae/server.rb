5b66dee466151205c0531321e9ae1a213f415e0a
Noah K. Tilton <binaural@gmail.com>
2010-11-08 21:12:17 -0600

    headers working

--- lib/reggae/server.rb
+++ lib/reggae/server.rb
@@ -1,45 +1,57 @@
 module Reggae
 
   class Server < EM::Connection
+
     include EM::HttpServer
-    #   signature: #{@signature}
-    #   Protocol: #{@http_protocol}
-    #   Request method: #{@http_request_method}
-    #   path info: #{@http_path_info}
-    #   request_uri: #{@http_request_uri}
-    #   proto: #{@http_protocol}
-    # called by the event loop immediately after network connection
-    # established, but before resumption of network loop
+
+    def initialize host, port
+      @host, @port = host, port
+    end
+
     def post_init
       super
       no_environment_strings
       @start_time = Time.now
       @header_processing = true
-      #@streamer = Streamer.new
     end
 
-    def set_response_headers
-      @response.status = 200
-      @response.content_type 'audio/x-mpegurl'
+    def request_fingerprint
+      puts [@signature, @http_protocol, @http_request_method,
+        @http_path_info, @http_request_uri].join " "
+    end
+
+    def set_client_headers
+      @client_headers = Hash[*@http_headers.split("\x00").map do |h|
+        h.split(/:\s+/,2)
+      end.flatten]
+    end
+
+    def send_server_headers
+      # see:    evma_httpserver/response.rb
       {
         'icy-notice1'   => 'reggae',
         'icy-notice2'   => 'reggae server',
-        'icy-name'      => 'reggae on #{gethostbyname}',
+        'icy-name'      => 'reggae on host',
         'icy-genre'     => 'pfunk',
         'icy-url'       => 'http://downbe.at:57715',
         'icy-pub'       => false,
         'icy-metaint'   => 16384
-      }.each { |h,v| send_data "#{h}: #{v}\r\n" }
+      }.each { |h,v| @response.headers[h] = v}
+      @response.content_type 'audio/x-mpegurl'
+      @response.status = 200    # setting status implicitly calls send_header
+      #@response.send_headers   # <-- won't work
     end
 
     def process_http_request
-      @headers = Hash[*@http_headers.split("\x00").map{|h|h.split(/:\s+/,2)}.flatten]
+      request_fingerprint
+      exit (-1) if @http_request_uri != '/'
       @response = EM::DelegatedHttpResponse.new self
-      set_response_headers
+      set_client_headers
+      send_server_headers
 
       case @http_request_method.upcase
         when 'GET' then
-          @response.send_response
+          # @response.send_response
           # Reggae::Streamer.new 
         when 'POST' then
           puts 'got a post'
@@ -51,5 +63,6 @@ module Reggae
     def unbind
       puts "disconnected"
     end
+
   end
 end
--- reggae.rb
+++ reggae.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 $:.push File.expand_path(File.dirname(__FILE__) + '/lib')
-%w[pp rubygems eventmachine evma_httpserver reggae].each{|lib| require lib}
+%w[pp rubygems eventmachine evma_httpserver reggae socket].each{|lib| require lib}
 
 EM.run do |conn|
   port = 57715 #     REGGAE
   host = '0.0.0.0'
-  EM.start_server host, port, Reggae::Server
+  EM.start_server host, port, Reggae::Server, host, port
   puts "{ reggae  http://#{host}:#{port} }"
 end
